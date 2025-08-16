595c5469894cea20379663314bb429fe96d488cc
ritesh-chauhan0x1 <22054368@kiit.ac.in>
2025-08-17 04:31:30 +0530

    Initial commit: Food-Delivery

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to "Food-Delivery"</h1>
+        <p>"E-commerce mobile app"</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
